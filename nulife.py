c4b6add95f552bacd1f0420d101f97727dbc7740
Aaron Pardes <aaronpardes@gmail.com>
2014-04-28 02:53:14 -0400

    mostly working, hacky front end implemented

--- nulife.py
+++ nulife.py
@@ -23,8 +23,9 @@ def testpoint():
 	return jsonify(name = name)
 
 class nuForm(Form):
-	zip_code = IntegerField('zip_code')
+	zipcode = IntegerField('zipcode')
 	salary = IntegerField('salary')
+	trans = IntegerField('trans')
 
 
 def remove(item):
@@ -35,7 +36,6 @@ def remove(item):
 	i3, i4 = i2.split(',')
 	i3 = float(i3)
 	return i3
-
 
 def zippo(zipcode):
 	url = 'http://api.zippopotam.us/us/'
@@ -165,16 +165,57 @@ def wolfram(city, state_abbr):
 
 	hneat = hraw.splitlines()
 
+	electricity = search('electricity price \\| {:f}', eraw).fixed
+	nat = search('natural gas price \\| ${:f} per', eraw).fixed
+
+	electricity = " " + str(electricity)
+	electricity = float(remove(electricity))
+
+	natural = " " + str(nat)
+	natural = float(remove(natural))
+
 	housing_prices = []
 
 	f = open('col_index2.json')
 	d = f.read()
 	data = json.loads(d)
 
+	i = 0
+	milk_summ = 0
+	utility_summ = 0
+	for item in data['results']['collection1']:
+		shitty, abbr = item['location'].split(', ')
+		if abbr == state_abbr:
+
+			utility_index = float(item['utilities_index'])
+			#print utility_index
+			milk_index = float(item['grocery_index'])
+
+			utility_summ = utility_summ + utility_index
+			milk_summ = milk_summ + milk_index
+			i += 1
+
+	utility_avg = utility_summ/i
+	#print utility_avg
+	milk_avg = milk_summ/i
+
+	base_milk = app.config['US_MILK_AVG']
+	base_util = app.config['US_UTIL_AVG']
+
+	milk_percent = milk_avg/100
+	local_milk = float(base_milk) * milk_percent
+
+	utility_percent = utility_avg/100
+	local_utilities = float(base_util) * utility_percent
+
+	local_milk = "{0:.2f}".format(local_milk)
+	local_utilities = "{0:.2f}".format(local_utilities)
+
+
 	inc1 = 1
 
 	for item in hneat:
-		if inc1 == 5:
+		if inc1 == 2:
 			break
 
 		item = " " + str(item)
@@ -207,15 +248,24 @@ def wolfram(city, state_abbr):
 				housing_prices.append(x)
 
 
+			#i = 0
+			#avg_house = 0
 
-			final_data = {'energy_raw' : energy_raw, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices} 
+			#for x in housing:
+			#	avg_house += x
+			#	i += 1
+
+			#avg_house = avg_house/i
+
+
+			final_data = {'natural' : natural, 'electricity' : electricity,'energy_raw' : eraw, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices, 'hneat' : hneat} 
 
 			return final_data
 
-		housing_prices.append(please2)
+		housing_prices.append(regex)
 		incr = inc1 + 1
 
-	final_data = {'energy_raw' : energy_raw, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices, 'hneat' : hneat}
+	final_data = {'natural' : natural, 'electricity' : electricity, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices, 'hneat' : hneat}
 
 	return final_data
 
@@ -223,182 +273,170 @@ def pubtrans():
 	pass
 
 
-def calculate(zip_code):
 
-	user_state = state_abbr
-	i = 0
-	milk_summ = 0
-	utility_summ = 0
-	for item in data['results']['collection1']:
-		shitty, abbr = item['location'].split(', ')
-		if abbr == user_state:
-
-			utility_index = float(item['utilities_index'])
-			#print utility_index
-			milk_index = float(item['grocery_index'])
-
-			utility_summ = utility_summ + utility_index
-			milk_summ = milk_summ + milk_index
-			i += 1
-
-	utility_avg = utility_summ/i
-	#print utility_avg
-	milk_avg = milk_summ/i
-
-	milk_percent = milk_avg/100
-	local_milk = float(base_milk) * milk_percent
-
-	utility_percent = utility_avg/100
-	local_utilities = float(base_util) * utility_percent
-
-	local_milk = "{0:.2f}".format(local_milk)
-	local_utilities = "{0:.2f}".format(local_utilities)
-
-	#### Wolfram API Calls
-
-	hous_neat = housing_raw.splitlines()
-
-	print hous_neat
-
-	housing_prices = []
-
-
-	incr = 1
-
-	for item in hous_neat:
-		if incr == 6:
-			break
-
-		item2 = " " + str(item)
-		please = search('\\\\| ${:d} per month  (US dollars per month)', item2)
-		please2 = remove(str(please))
-
-		if please2 == False:
-			ic = 0
-			housing_sum = 0
-			us_fmr = app.config['US_FMR']
-			for item in data['results']['collection1']:
-				print item
-				shitty, abbr  = item['location'].split(', ')
-				print abbr
-				if abbr == user_state:
-					print user_state
-					housing_index = float(item['housing_index'])
-					print housing_index
-					housing_sum = housing_sum + housing_index
-
-				ic = ic + 1
-				print ic
-			housing_avg = housing_sum/ic
-			print housing_avg
-			housing_percent = housing_avg/100
-			print housing_percent
-
-			housing_prices = []
-
-			for x in us_fmr:
-				x = x * housing_percent
-				print x
-				housing_prices.append(x)
-
-
-
-			final_data = {'energy_raw' : energy_raw, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices} 
-
-			return final_data
-
-		print please2
-		housing_prices.append(please2)
-		print incr
-		incr = incr + 1
-
-
-	print housing_prices
-
-
-
-	#results(energy_final, local_utilities, local_milk)
-
-	final_data = {'energy_raw' : energy_raw, 'local_utilities' : local_utilities, 'local_milk' : local_milk, 'housing_prices' : housing_prices}
-
-	return final_data
 
 
 @app.route('/liftoff', methods = ['POST'])
-def liftOff(request):
-	monthly_salary = salary/12
-	sal_af_tax = monthly_salary * .72
-	saved_monthly = sal_af_tax * .15
-	gas_avg = app.config['US_GAS_AVG']
-	avg_cable = 150
-	month_bal = sal_af_tax - saved_monthly
+def liftOff():
+	salary = float(request.values['salary'])
+	print salary	
+	transportation = 0.00
+	monthly_expenses = 0.00
 
-	cars = {1:10, 2:15, 3:20}
+	monthly_sal = salary / float(12)
+	monthly_sal_af_tax = monthly_sal * .72
+	monthly_savings = monthly_sal_af_tax * .15
 
-	if carbool == True:
+	### Get city/state from zip or (if provided) just proceed
+	try:
+		zipcode = request.values['zipcode']
+		citystate = zippo(zipcode)
+		citystate = json.loads(citystate)
+		city = citystate['city']
+		state_abbr = citystate['state_abbr']
 
-		tank = cars[cartype]
-		gas_month = tank * 2 * gas_avg
-		month_bal = month_bal - gas_month - insurance
-
-
-		if paymentsbool == True:
-			month_bal -= payments
-			
-	else:
-		month_bal -= trans
-
-
-	if cellbool == True:
-		month_bal -= cell
-
-	########### LIFTOFF ##########
-
-	if lifechoice == True:
-
-		avg_groceries = app.config['AVG_GROCERIES']
-		month_bal -= avg_groceries
-		data = calculate(zipcode)	
-
-		housing = data['housing_prices']
-
-		i = 0
-		avg_house = 0
-
-		for x in housing:
-			avg_house += x
-			i += 1
-
-		avg_house = avg_house/i
-
-		energy = data['energy_raw']
-		elect = search('electricity price \\| {:f}', energy).fixed
-		natura = search('natural gas price \\| ${:f} per', energy).fixed
-
-		#hous_neat = hous_raw.splitlines()
-		
-		elec = " " + str(elect)
-		elec = float(remove(elec))
-
-		natural = " " + str(natura)
-		natural = float(remove(natural))
-
-		milk = float(data['local_milk'])
-		utilities = float(data['local_utilities'])
-
-		month_bal = month_bal - float(utilities)
-
-		return json.dumps({'milk':milk, 'utilities':utilities, 'natural':natural, 'elec':elec, 'sal_af_tax':sal_af_tax, 'saved_monthly':saved_monthly, 'month_bal':month_bal})
+	except KeyError:
+		city = request.values['city']
+		state_abbr = request.values['state']
 
 
-		########### GET ORGANIZED ##########
+	wdata = wolfram(city, state_abbr)
 
-	else:
-		
-		month_bal -= groceries
-		month_bal = month_bal - elec - nat - gas
+	print 'car stuff'
+	### Car stuff
+	try:
+		cartype = request.values['cartype']
+		insurance_year = float(request.values['insurance'])
+		insurance = insurance_year / float(12)
+		try:
+			payments = float(request.values['payments'])
+			monthly_expenses = payments
 
-		return jsonify(milk = milk, sal_af_tax = sal_af_tax, saved_monthly = saved_monthly, month_bal = month_bal)
+		except KeyError:
+			payments = 0
+			print "uh oh"
+
+		car_month = car(float(cartype), payments, insurance)
+		print "\nCar Month: ${}".format(car_month)
+		transportation = car_month
+		monthly_expenses = monthly_expenses + transportation
+
+	except KeyError:
+		transportation = float(request.values['trans'])
+		monthly_expenses = monthly_expenses + transportation
+		print monthly_expenses
+
+	print 'phone stuff'
+	### Cell phone stuff
+	try:
+		cell = float(request.values['cell'])
+		monthly_expenses = monthly_expenses + cell
+		print monthly_expenses
+	except KeyError:
+		pass
+
+	avg_groceries = app.config['AVG_GROCERIES']
+	monthly_expenses = monthly_expenses + avg_groceries
+
+	monthly_bal = monthly_sal_af_tax - monthly_savings - monthly_expenses
+
+	natural_gas = 'Natural Gas: ${}/thousand cubic meters'.format(wdata['natural'])
+
+	electricity = 'Electricity: ${}/kWh'.format(wdata['electricity'])
+
+	milk = 'Local Price of 1 Gallon of Milk: ${}'.format(wdata['local_milk'])
+
+	local_utils = 'Average Local Monthly Utilities in this Area: ${}'.format(wdata['local_utilities'])
+
+	neat_housing = wdata['hneat']
+
+	housing_prices = wdata['housing_prices']
+
+	return jsonify(monthly_bal = monthly_bal, transportation = transportation, monthly_sal_af_tax = monthly_sal_af_tax, monthly_savings = monthly_savings, monthly_expenses = monthly_expenses)
+
+
+
+@app.route('/ores', methods = ['POST'])
+def liftOffOnline():
+	salary = float(request.values['salary'])
+	print salary	
+	transportation = 0.00
+	monthly_expenses = 0.00
+
+	monthly_sal = salary / float(12)
+	monthly_sal_af_tax = monthly_sal * .72
+	monthly_savings = monthly_sal_af_tax * .15
+	print monthly_savings
+
+	### Get city/state from zip or (if provided) just proceed
+	try:
+		zipcode = request.values['zipcode']
+		citystate = zippo(zipcode)
+		citystate = json.loads(citystate)
+		city = citystate['city']
+		state_abbr = citystate['state_abbr']
+
+	except KeyError:
+		print "ok"
+		city = request.values['city']
+		state_abbr = request.values['state']
+		print state_abbr
+
+
+	wdata = wolfram(city, state_abbr)
+
+	print 'car stuff'
+	### Car stuff
+	try:
+		cartype = request.values['cartype']
+		insurance_year = float(request.values['insurance'])
+		insurance = insurance_year / float(12)
+		try:
+			payments = float(request.values['payments'])
+			monthly_expenses = payments
+
+		except KeyError:
+			payments = 0
+			print "uh oh"
+
+		car_month = car(float(cartype), payments, insurance)
+		print "\nCar Month: ${}".format(car_month)
+		transportation = car_month
+		monthly_expenses = monthly_expenses + transportation
+
+	except KeyError:
+		transportation = float(request.values['trans'])
+		monthly_expenses = monthly_expenses + transportation
+		print monthly_expenses
+
+	print 'phone stuff'
+	### Cell phone stuff
+	try:
+		cell = float(request.values['cell'])
+		monthly_expenses = monthly_expenses + cell
+		print monthly_expenses
+	except KeyError:
+		pass
+
+	avg_groceries = app.config['AVG_GROCERIES']
+	monthly_expenses = monthly_expenses + avg_groceries
+
+	monthly_bal = monthly_sal_af_tax - monthly_savings - monthly_expenses
+
+	natural_gas = 'Natural Gas: ${}/thousand cubic meters'.format(wdata['natural'])
+
+	electricity = 'Electricity: ${}/kWh'.format(wdata['electricity'])
+
+	milk = 'Local Price of 1 Gallon of Milk: ${}'.format(wdata['local_milk'])
+
+	local_utils = 'Average Local Monthly Utilities: ${}'.format(wdata['local_utilities'])
+
+	neat_housing = wdata['hneat']
+
+	housing_prices = wdata['housing_prices']
+
+	return render_template('ores.html', state_abbr = state_abbr, city = city, electricity = electricity, milk = milk, local_utils = local_utils, neat_housing = neat_housing , natural_gas = natural_gas, monthly_bal = monthly_bal, transportation = transportation, monthly_sal_af_tax = monthly_sal_af_tax, monthly_savings = monthly_savings, monthly_expenses = monthly_expenses)
+
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -409,24 +447,24 @@ def index():
 		zip_code = request.form['zip_code']
 		salary = request.form['salary']
 		print salary
-		data = calculate(zip_code)
-		energy = data['energy_raw']
+		#data = calculate(zip_code)
+		#energy = data['energy_raw']
 		
 		#hous_raw = data['housing_raw']
 		
-		elec = search('electricity price \\| {:f}', energy).fixed
-		nat = search('natural gas price \\| ${:f} per', energy).fixed
+		#elec = search('electricity price \\| {:f}', energy).fixed
+		#nat = search('natural gas price \\| ${:f} per', energy).fixed
 
 		#hous_neat = hous_raw.splitlines()
 		
-		elec = " " + str(elec)
-		elec = remove(elec)
-		nat = " " + str(nat)
-		nat = remove(nat)
-		milk = data['local_milk']
-		utilities = data['local_utilities']
+		#elec = " " + str(elec)
+		#elec = remove(elec)
+		#nat = " " + str(nat)
+		#nat = remove(nat)
+		#milk = data['local_milk']
+		#utilities = data['local_utilities']
 
-		return render_template('results.html', elec = elec, milk = milk, utilities = utilities, nat = nat )
+		return render_template('results.html')#, elec = elec, milk = milk, utilities = utilities, nat = nat )
 
 	else:
 
@@ -442,6 +480,20 @@ def results(energy, utilities, milk):
 	return render_template('results.html', energy=energy)
 
 
+@app.route('/test', methods = ['GET', 'POST'])
+def testRoute():
+	
+	form = nuForm()
+	if request.method == 'POST':
+		salary = float(request.values['salary'])
+		zipcode = float(request.values['zip_code'])
+		trans = float(request.values['trans'])
+
+		r = liftOff(salary, zipcode, trans)
+
+		return render_template('results.html', r = r)
+	else:
+		return render_template('test.html', form = form)
 
 
 
